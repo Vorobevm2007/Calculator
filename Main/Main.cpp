e544fc347d1156cd5f955220be6e6cf2cbb4694f
Vorobevm2007 <matvejvorobev255@gmail.com>
2025-08-12 16:49:08 +0300

    Initial commit

---
+++ Main/Main.cpp
@@ -0,0 +1,90 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <string>
+
+class Calculator
+{
+public:
+    char EnterSign(char arrSigns[], const size_t SIGNS_COUNT)
+    {
+        char sign;
+        bool flag = 1;
+        while (flag)
+        {
+            std::cout << "Введите метод операции (+|-|*|/):";
+            std::cin >> sign;
+            for (int i=0;i < SIGNS_COUNT;i++)
+            {
+                if (sign == arrSigns[i]) { flag = 0; }
+            }
+            if (flag) { CheckForExceptions(); }
+        }
+        return sign;
+    }
+    double EnterNumber()
+    {
+        float number;
+        while (std::cout << "Введи число:" && !(std::cin >> number)) {
+            CheckForExceptions();
+        }
+        return number;
+    }
+    void CheckForExceptions()
+    {
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cout << "error\n";
+    }
+    double SolveExpressione(double x, char sign, double y)
+    {
+        switch (sign)
+        {
+            case '/':
+                if (y != 0) {
+                    return x / y;
+                }
+                else {
+                    std::cout << "Деление на 0!!!" << std::endl;
+                    return 0;
+                }
+                break;
+            case '*':
+                return x * y;
+                break;
+            case '-':
+                return x - y;
+                break;
+            case '+':
+                return x + y;
+                break;
+        }
+    }
+    void PrintAndSolveExpression(double x,char sign, double y)
+    {
+        std::cout << x << sign << y << '=' << SolveExpressione(x, sign, y) << std::endl;
+    }
+};
+
+
+int main()
+{
+    system("chcp 1251>nul");
+
+    const size_t SIGNS_COUNT = 4;
+    char arrSigns[SIGNS_COUNT]{ '/', '*', '-', '+' };
+    Calculator calculator;
+
+    double x = 0,y = 0;
+    char sign = 0;
+
+    while (true)
+    {
+        x = calculator.EnterNumber();
+        sign = calculator.EnterSign(arrSigns,SIGNS_COUNT);
+        y = calculator.EnterNumber();
+        calculator.PrintAndSolveExpression(x, sign, y);
+    }
+
+    return 0;
+}
+
